0853a83a5528f68a9c725e0c5a6a783258d3788d
Fabian Vogelsteller <fabian@frozeman.de>
2020-03-27 11:19:16 +0100

    commented committEth

--- contracts/ReversibleICO.sol
+++ contracts/ReversibleICO.sol
@@ -145,7 +145,7 @@ contract ReversibleICO is IERC777Recipient {
         uint256 NEWreservedTokens;
         uint256 NEWunlockedTokens; // NEEDED?
         uint256 NEWpendingEth;
-        uint256 NEWcommittedEth;
+//        uint256 NEWcommittedEth;
 //        uint256 NEWallocatedEth;
         uint256 NEWlastBlock;
     }
@@ -634,8 +634,8 @@ contract ReversibleICO is IERC777Recipient {
     function getParticipantDetailsByStage(address _address, uint8 _stageId) public view
     returns (
         uint256 NEWreservedTokens,
-        uint256 NEWpendingEth,
-        uint256 NEWcommittedEth
+        uint256 NEWpendingEth
+//        uint256 NEWcommittedEth
 //        uint256 NEWallocatedEth
 
 //        uint256 stagetotalSentETH,
@@ -652,8 +652,8 @@ contract ReversibleICO is IERC777Recipient {
         ParticipantStageDetails storage totalsRecord = participantsByAddress[_address].byStage[_stageId];
         return (
         totalsRecord.NEWreservedTokens,
-        totalsRecord.NEWpendingEth,
-        totalsRecord.NEWcommittedEth
+        totalsRecord.NEWpendingEth
+//        totalsRecord.NEWcommittedEth
 //        totalsRecord.NEWallocatedEth
 
 //        totalsRecord.totalSentETH,
