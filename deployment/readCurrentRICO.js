fb96f8631a61388ec05e3f6b66c5c30e62c4415a
Fabian Vogelsteller <fabian@frozeman.de>
2020-06-11 09:28:21 +0200

    improved read SC

--- deployment/readCurrentRICO.js
+++ deployment/readCurrentRICO.js
@@ -28,7 +28,7 @@ let web3 = new Web3(config.settings.provider);
 let run = async function() {
     const abi = await utils.getAbi("ReversibleICO");
 
-    let rICOcontract = new web3.eth.Contract(abi, '0x34a34267ec4034ba52f154829d0b6548bbbf7c74', {gas: '6500000', gasPrice: '200000000'});
+    let rICOcontract = new web3.eth.Contract(abi, '0xE417b912F6cB6592ec2D71dbF6F2B48191b2CdF6', {gas: '6500000', gasPrice: '400000000'});
 
     let participantsId = await rICOcontract.methods.participantCount().call();
     console.log('participantsId', participantsId);
@@ -45,16 +45,22 @@ let run = async function() {
         participantRequests.push(rICOcontract.methods.participants(partici).call);
     });
     let participants = await makeBatchRequest(web3, participantRequests);
-
-    // add lister
-    web3.eth.accounts.wallet.add('0x');
-    let acc = web3.eth.accounts.wallet[0].address;
-    let nonce = await web3.eth.getTransactionCount(acc);
-    console.log(acc, nonce);
+    // let participants = require('../WHITELISTED_participants_2020-06-05T15-51:51-942Z.json');
+    //
+    //
+    // // add lister
+    // web3.eth.accounts.wallet.add('0x');
+    // let acc = web3.eth.accounts.wallet[0].address;
+    // let nonce = await web3.eth.getTransactionCount(acc);
+    // console.log(acc, nonce);
 
     let list = [];
     participants.forEach((partici, i)=>{
-        partici.address = participantsAddresses[i];
+        // partici.address = participantsAddresses[i];
+
+        if(partici.whitelisted){
+            list.push(partici.address);
+        }
 
         if(partici.committedETH > 0) {
             // console.log('committedETH', partici.address);
@@ -62,21 +68,22 @@ let run = async function() {
             // list.push(partici.address);
         }
         if(partici.pendingETH > 0) {
-            console.log('pendingETH', partici.address);
+            // console.log('pendingETH', partici.address);
 
-            list.push(partici.address);
+            // list.push(partici.address);
         }
 
     });
 
     console.log(list.length);
+    console.log(list);
 
-    // await rICOcontract.methods.whitelist(list, false).send({
-    //     from: acc,
-    //     gas: '850000',
-    //     gasPrice: '40000000000',
-    //     // nonce: nonce++
-    // });
+    await rICOcontract.methods.whitelist(list, true).send({
+        from: acc,
+        gas: '1250000',
+        gasPrice: '40000000000',
+        // nonce: nonce++
+    });
 
 
     // fs.writeFileSync('./participants_'+ new Date().toISOString().replace(':','-').replace('.','-') +'.json', JSON.stringify(participants, null, 4) ,{encoding:'utf8',flag:'w'});
